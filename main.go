b921b70dfbca6baac366f2c3aac832ab1b58f0bb
Meysam Azad <meysam@developer-friendly.blog>
2025-06-14 14:19:22 +0700

    feat: add goreleaser

--- main.go
+++ main.go
@@ -24,6 +24,8 @@ var (
 
 type AppState struct {
 	l *logging.Logger
+	c *Config
+	h *HttpCheck
 }
 
 type Config struct {
@@ -60,24 +62,26 @@ func randomBetween(min, max int) int {
 	return rand.Intn(max-min+1) + min
 }
 
-func newLogger(c *Config) *logging.Logger {
-	logger := logging.NewLogger(logging.WithLogLevel(translateLogLevel(c.logLevel)))
+func newLogger(level string) *logging.Logger {
+	logger := logging.NewLogger(logging.WithLogLevel(translateLogLevel(level)))
 	return &logger
 }
 
-func (a *AppState) performHttpCheck(check *HttpCheck) error {
+func (a *AppState) performHttpCheck() error {
 	client := &http.Client{
-		Timeout: time.Duration(check.timeout) * time.Second,
+		Timeout: time.Duration(a.h.timeout) * time.Second,
 	}
 
 	var tries uint = 0
 	for {
-		if check.retries > 0 && tries >= check.retries {
-			a.l.Error().Msgf("halting check with max retries reached: %d", check.retries)
+		if a.h.retries > 0 && tries >= a.h.retries {
+			a.l.Error().Msgf("halting check with max retries reached: %d", a.h.retries)
 			return nil
 		}
 
-		resp, err := client.Get(check.upstream)
+		start := time.Now()
+
+		resp, err := client.Get(a.h.upstream)
 		if err != nil {
 			tries += 1
 			jitter := randomBetween(5, 10)
@@ -86,8 +90,10 @@ func (a *AppState) performHttpCheck(check *HttpCheck) error {
 			continue
 		}
 
-		if resp.StatusCode == int(check.statusCode) {
-			a.l.Info().Msgf("http check successful with status code: %s", resp.Status)
+		elapsed := time.Since(start)
+
+		if resp.StatusCode == int(a.h.statusCode) {
+			a.l.Info().Msgf("took %s for http check successful with status code: %s", elapsed.Round(time.Millisecond).String(), resp.Status)
 			break
 		}
 	}
@@ -95,7 +101,7 @@ func (a *AppState) performHttpCheck(check *HttpCheck) error {
 	return nil
 }
 
-func (a *AppState) createCheckCommand(config *Config, httpCheck *HttpCheck) *cli.Command {
+func (a *AppState) createCheckCommand() *cli.Command {
 	return &cli.Command{
 		Name:  "check",
 		Usage: "Perform the HTTP check",
@@ -105,56 +111,12 @@ func (a *AppState) createCheckCommand(config *Config, httpCheck *HttpCheck) *cli
 				Aliases:     []string{"u"},
 				Usage:       "The http target/upstream in the format http://my-service.com/healthz",
 				Required:    true,
-				Destination: &httpCheck.upstream,
+				Destination: &a.h.upstream,
 				Sources:     cli.EnvVars("HTTP_TARGET"),
-			},
-			&cli.UintFlag{
-				Name:        "retries",
-				Aliases:     []string{"r"},
-				Usage:       "How many times to retry the healthcheck. Pass 0 for infinite tries.",
-				Value:       0,
-				Destination: &httpCheck.retries,
-				Sources:     cli.EnvVars("RETRIES"),
-			},
-			&cli.UintFlag{
-				Name:        "timeout",
-				Aliases:     []string{"t"},
-				Usage:       "Seconds to wait for each check before considering it a failure.",
-				Value:       5,
-				Destination: &httpCheck.timeout,
-				Sources:     cli.EnvVars("TIMEOUT"),
-			},
-			&cli.UintFlag{
-				Name:        "status-code",
-				Aliases:     []string{"c"},
-				Usage:       "The status to check for when sending HTTP request",
-				Value:       http.StatusOK,
-				Destination: &httpCheck.statusCode,
-				Sources:     cli.EnvVars("STATUS_CODE"),
-			},
-			&cli.StringFlag{
-				Name:        "log-level",
-				Aliases:     []string{"l"},
-				Usage:       "The verbosity of the logs (debug, info, warn, error, critical)",
-				Value:       "info",
-				Destination: &config.logLevel,
-				Sources:     cli.EnvVars("LOG_LEVEL"),
 			},
 		},
 		Action: func(ctx context.Context, c *cli.Command) error {
-			logLevel := strings.ToLower(config.logLevel)
-			switch logLevel {
-			case "debug":
-			case "info":
-			case "warn":
-			case "error":
-			case "critical":
-				a.l = newLogger(config)
-			default:
-				return fmt.Errorf("unknown log level provided: %s, accepted log levels are debug, info, warn, error, critical", config.logLevel)
-			}
-
-			return a.performHttpCheck(httpCheck)
+			return a.performHttpCheck()
 		},
 	}
 }
@@ -162,10 +124,9 @@ func (a *AppState) createCheckCommand(config *Config, httpCheck *HttpCheck) *cli
 func main() {
 	ctx := context.Background()
 
-	httpCheck := &HttpCheck{}
 	config := &Config{logLevel: "info"}
-	logger := newLogger(config)
-	app := AppState{l: logger}
+	logger := newLogger(config.logLevel)
+	app := AppState{l: logger, c: config, h: &HttpCheck{}}
 
 	cli.VersionPrinter = func(c *cli.Command) {
 		fmt.Printf("Version:    %s\n", version)
@@ -182,10 +143,69 @@ func main() {
 		Suggest:               true,
 		EnableShellCompletion: true,
 		Commands: []*cli.Command{
-			app.createCheckCommand(config, httpCheck),
+			app.createCheckCommand(),
+		},
+		Flags: []cli.Flag{
+			&cli.UintFlag{
+				Name:        "retries",
+				Aliases:     []string{"r"},
+				Usage:       "How many times to retry the healthcheck. Pass 0 for infinite tries.",
+				Value:       0,
+				Destination: &app.h.retries,
+				Sources:     cli.EnvVars("RETRIES"),
+			},
+			&cli.UintFlag{
+				Name:        "timeout",
+				Aliases:     []string{"t"},
+				Usage:       "Seconds to wait for each check before considering it a failure.",
+				Value:       5,
+				Destination: &app.h.timeout,
+				Sources:     cli.EnvVars("TIMEOUT"),
+			},
+			&cli.UintFlag{
+				Name:        "status-code",
+				Aliases:     []string{"c"},
+				Usage:       "The status to check for when sending HTTP request",
+				Value:       http.StatusOK,
+				Destination: &app.h.statusCode,
+				Sources:     cli.EnvVars("STATUS_CODE"),
+			},
+			&cli.StringFlag{
+				Name:        "log-level",
+				Aliases:     []string{"l"},
+				Usage:       "The verbosity of the logs (debug, info, warn, error, critical)",
+				Value:       "info",
+				Destination: &app.c.logLevel,
+				Sources:     cli.EnvVars("LOG_LEVEL"),
+			},
 		},
 		Version: version,
+		Action: func(ctx context.Context, c *cli.Command) error {
+			logLevel := strings.ToLower(app.c.logLevel)
+			switch logLevel {
+			case "debug":
+			case "info":
+			case "warn":
+			case "error":
+			case "critical":
+				app.l = newLogger(app.c.logLevel)
+			default:
+				return fmt.Errorf("unknown log level provided: %s, accepted log levels are debug, info, warn, error, critical", app.c.logLevel)
+			}
+
+			if c.NArg() == 0 {
+				availableCommands := []string{}
+				for _, subcommand := range c.Commands {
+					availableCommands = append(availableCommands, subcommand.Name)
+				}
+				return fmt.Errorf("no command provided. available subcommands: %s", strings.Join(availableCommands, ", "))
+			}
+
+			return nil
+		},
 	}
+
+	app.l.Debug().Msg("Starting the app...")
 
 	err := cmd.Run(ctx, os.Args)
 	if err != nil {
